0901a8b30354da1c4a4527b84d205c36a7772d82
RAVITEJA GUNTY <ravitejagunty@gmail.com>
2025-01-13 17:50:45 -0500

    program structure, adding expressions, statements, functions and bindings

---
+++ program-structure/expressions-statements/expressions-statements.js
@@ -0,0 +1,14 @@
+/* A fragment of code that generate as value is called 'expression'
+example: 1; , output: 1
+*/
+
+/* statement on the other hand is a full sentence
+example: in english - if 5 less than 4 return true else return false, in javascript - 
+*/
+if 5 < 4
+    return true
+else
+    return false
+/*
+output:  true
+*/
---
+++ program-structure/functions/functions.js
@@ -0,0 +1,14 @@
+/* A function is piece of program wrapped in a value */
+
+prompt('Mastering Javascript');
+//ouput: Mastreing Javascript
+
+/* Here prompt is a function which is an in built function that allow to display whatever the outcome is with in the brackets(), 
+one can write any executable logic with in a function;
+executing a function is often referred to invoking or calling */
+
+console.log('Mastering Javascript');
+//output: Mastering Javascript
+
+/* console.log is often referred as function in actual it isn't, console is a binding and log is a proprety of that binding, so console.log is actually an expression
+than a function */
